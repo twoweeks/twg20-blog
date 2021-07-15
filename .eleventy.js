208b41ff6b68d15f3ad88c66e0628b3c88738011
Dmitry <git@e965.ru>
2021-07-15 17:18:34 +0500

    fix browsersync

--- .eleventy.js
+++ .eleventy.js
@@ -91,7 +91,7 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.setBrowserSyncConfig({
     callbacks: {
       ready: function(err, browserSync) {
-        const content_404 = fs.readFileSync('dist/site/404.html');
+        const content_404 = fs.readFileSync('dist/404.html');
 
         browserSync.addMiddleware("*", (req, res) => {
           // Provides the 404 content without redirect.

2b60c7db4ebeb2de14775a7e6f8d73f1db590afc
Dmitry <git@e965.ru>
2022-02-07 10:57:19 +0500

    fix links

--- .eleventy.js
+++ .eleventy.js
@@ -2,37 +2,40 @@ const fs = require("fs");
 
 const pluginRss = require("@11ty/eleventy-plugin-rss");
 const pluginNavigation = require("@11ty/eleventy-navigation");
-const safeExternalLinks = require("@hirusi/eleventy-plugin-safe-external-links");
+const externalLinks = require("@aloskutov/eleventy-plugin-external-links");
 
 const markdownIt = require("markdown-it");
 const markdownItAnchor = require("markdown-it-anchor");
-const htmlmin = require('html-minifier');
+const htmlmin = require("html-minifier");
 
-module.exports = function(eleventyConfig) {
+module.exports = function (eleventyConfig) {
   // Add plugins
   eleventyConfig.addPlugin(pluginRss);
   eleventyConfig.addPlugin(pluginNavigation);
 
-  eleventyConfig.addPlugin(safeExternalLinks, {
-    noreferrer: true,
-  });
+  eleventyConfig.addPlugin(externalLinks);
 
   // Alias `layout: post` to `layout: layouts/post.njk`
   eleventyConfig.addLayoutAlias("post", "layouts/post.njk");
 
-  eleventyConfig.addFilter("readableDate", dateObj => {
-    return new Date(dateObj).toLocaleString('ru', { dateStyle: 'medium' });
+  eleventyConfig.addFilter("readableDate", (dateObj) => {
+    return new Date(dateObj).toLocaleString("ru", { dateStyle: "medium" });
   });
 
-  eleventyConfig.addFilter('htmlDateString', (dateObj) => {
-    const _date = new Date(dateObj)
-    const month = new Date().getMonth() + 1
+  eleventyConfig.addFilter("htmlDateString", (dateObj) => {
+    const _date = new Date(dateObj);
+    const month = new Date().getMonth() + 1;
 
-    return `${_date.getFullYear()}-${month < 10 ? `0${month}` : month}-${_date.getDate()}`
+    return `${_date.getFullYear()}-${
+      month < 10 ? `0${month}` : month
+    }-${_date.getDate()}`;
   });
 
   // Get the first `n` elements of a collection.
   eleventyConfig.addFilter("head", (array, n) => {
+    if (!Array.isArray(array) || array.length === 0) {
+      return [];
+    }
     if (n < 0) {
       return array.slice(n);
     }
@@ -45,18 +48,16 @@ module.exports = function(eleventyConfig) {
     return Math.min.apply(null, numbers);
   });
 
-  eleventyConfig.addFilter('addTimestamp', (value) => {
-    return value + '?ts=' + new Date().getTime();
+  eleventyConfig.addFilter("addTimestamp", (value) => {
+    return value + "?ts=" + new Date().getTime();
   });
 
-  eleventyConfig.addTransform('htmlmin', (content, outputPath) => {
-    if (outputPath && outputPath.endsWith('.html')) {
-        return htmlmin.minify(
-            content, {
-                removeComments: true,
-                collapseWhitespace: true
-            }
-        );
+  eleventyConfig.addTransform("htmlmin", (content, outputPath) => {
+    if (outputPath && outputPath.endsWith(".html")) {
+      return htmlmin.minify(content, {
+        removeComments: true,
+        collapseWhitespace: true,
+      });
     }
     return content;
   });
@@ -64,53 +65,60 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addFilter("declOfNum", ({ number, titles }) => {
     const NumberAbs = Math.abs(number);
     const Cases = [2, 0, 1, 1, 1, 2];
-    return `${NumberAbs} ${titles[NumberAbs % 100 > 4 && NumberAbs % 100 < 20 ? 2 : Cases[NumberAbs % 10 < 5 ? NumberAbs % 10 : 5]]}`;
+    return `${NumberAbs} ${
+      titles[
+        NumberAbs % 100 > 4 && NumberAbs % 100 < 20
+          ? 2
+          : Cases[NumberAbs % 10 < 5 ? NumberAbs % 10 : 5]
+      ]
+    }`;
   });
 
   // Copy to the output
   eleventyConfig.addPassthroughCopy("src/styles");
-  eleventyConfig.addPassthroughCopy('src/scripts');
-  eleventyConfig.addPassthroughCopy('src/**/*.(html|gif|jpg|png|webp|ico|svg|mp4|webm|zip)');
+  eleventyConfig.addPassthroughCopy("src/scripts");
+  eleventyConfig.addPassthroughCopy(
+    "src/**/*.(html|gif|jpg|png|webp|ico|svg|mp4|webm|zip)"
+  );
 
   // Customize Markdown library and settings:
   let markdownLibrary = markdownIt({
     html: true,
     breaks: true,
-    linkify: true
+    linkify: true,
   }).use(markdownItAnchor, {
-    permalink: true,
-    permalinkClass: "direct-link",
-    permalinkSymbol: "#"
+    permalink: markdownItAnchor.permalink.ariaHidden({
+      placement: "after",
+      class: "direct-link",
+      symbol: "#",
+      level: [1, 2, 3, 4],
+    }),
+    slugify: eleventyConfig.getFilter("slug"),
   });
   eleventyConfig.setLibrary("md", markdownLibrary);
 
   // Override Browsersync defaults (used only with --serve)
   eleventyConfig.setBrowserSyncConfig({
     callbacks: {
-      ready: function(err, browserSync) {
-        const content_404 = fs.readFileSync('dist/404.html');
+      ready: function (err, browserSync) {
+        const content_404 = fs.readFileSync("dist/404.html");
 
         browserSync.addMiddleware("*", (req, res) => {
           // Provides the 404 content without redirect.
-          res.writeHead(404, {"Content-Type": "text/html; charset=UTF-8"});
+          res.writeHead(404, { "Content-Type": "text/html; charset=UTF-8" });
           res.write(content_404);
           res.end();
         });
       },
     },
     ui: false,
-    ghostMode: false
+    ghostMode: false,
   });
 
   return {
     // Control which files Eleventy will process
     // e.g.: *.md, *.njk, *.html, *.liquid
-    templateFormats: [
-      "md",
-      "njk",
-      "html",
-      "liquid"
-    ],
+    templateFormats: ["md", "njk", "html", "liquid"],
 
     // -----------------------------------------------------------------
     // If your site deploys to a subdirectory, change `pathPrefix`.
@@ -126,9 +134,9 @@ module.exports = function(eleventyConfig) {
     pathPrefix: "/",
     // -----------------------------------------------------------------
 
-    markdownTemplateEngine: 'njk',
-    htmlTemplateEngine: 'njk',
-    dataTemplateEngine: 'njk',
+    markdownTemplateEngine: "njk",
+    htmlTemplateEngine: "njk",
+    dataTemplateEngine: "njk",
 
     passthroughFileCopy: true,
 
@@ -137,7 +145,7 @@ module.exports = function(eleventyConfig) {
       input: "src",
       includes: "includes",
       data: "data",
-      output: "dist"
-    }
+      output: "dist",
+    },
   };
 };
